a81f86f979b8ac05ada45b845181c43e0407e5e0
PattyCutie <iamsureiamcute@gmail.com>
2022-01-03 19:37:14 +0100

    made changed some details

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -105,10 +105,10 @@ class Question(models.Model):
     # question text
     question_text = models.CharField(max_length=300)
     # question grade/mark
-    grade =models.IntegerField(default=10)
+    grade = models.IntegerField(default=10)
 
     # <HINT> A sample model method to calculate if learner get the score of the question
-def is_get_score(self, selected_ids):
+    def is_get_score(self, selected_ids):
         all_answers = self.choice_set.filter(is_correct=True).count()
         selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
         if all_answers == selected_correct:
@@ -123,7 +123,7 @@ def is_get_score(self, selected_ids):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 class Choice(models.Model):
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=300)
     is_correct = models.BooleanField(default=False)
 
